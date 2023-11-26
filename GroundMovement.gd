95127a092973694258d6e36c3886022b05bc1a4f
MrRuslanYT <pandacorpglobal@gmail.com>
2023-11-26 12:24:37 +0200

    Fixed collision end on ground blocks

--- GroundMovement.gd
+++ GroundMovement.gd
@@ -1,38 +1,37 @@
-extends StaticBody2D
+extends Node2D
 
-const GROUND_SCALE = 20.5706
+const GROUND_SCALE = 20.5
 const SPEED = 300
-onready var ground1: Sprite = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground1")
-onready var ground2: Sprite = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground2")
-onready var ground3: Sprite = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground3")
-onready var visibilityNotifier1: VisibilityNotifier2D = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground1/VisibilityNotifier2D")
-onready var visibilityNotifier2: VisibilityNotifier2D = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground2/VisibilityNotifier2D")
-onready var visibilityNotifier3: VisibilityNotifier2D = get_node("/root/StaticBody2D/StaticBody2D/CollisionShape2D/Ground3/VisibilityNotifier2D")
+onready var ground1: StaticBody2D = get_node("/root/StaticBody2D/Node2D/Ground1")
+onready var ground2: StaticBody2D = get_node("/root/StaticBody2D/Node2D/Ground2")
+onready var ground3: StaticBody2D = get_node("/root/StaticBody2D/Node2D/Ground3")
+onready var ground1_sprite: Sprite = get_node("/root/StaticBody2D/Node2D/Ground1/CollisionShape2D/Sprite")
+onready var ground2_sprite: Sprite = get_node("/root/StaticBody2D/Node2D/Ground2/CollisionShape2D/Sprite")
+onready var ground3_sprite: Sprite = get_node("/root/StaticBody2D/Node2D/Ground3/CollisionShape2D/Sprite")
+onready var visibilityNotifier1: VisibilityNotifier2D = get_node("/root/StaticBody2D/Node2D/Ground1/CollisionShape2D/VisibilityNotifier2D")
+onready var visibilityNotifier2: VisibilityNotifier2D = get_node("/root/StaticBody2D/Node2D/Ground2/CollisionShape2D/VisibilityNotifier2D")
+onready var visibilityNotifier3: VisibilityNotifier2D = get_node("/root/StaticBody2D/Node2D/Ground3/CollisionShape2D/VisibilityNotifier2D")
 var translation = Vector2.ZERO
 
-func moveGround():
-	if (!visibilityNotifier1.is_on_screen()):
-		var new_position = ground3.position
-		new_position.x += ground3.texture.get_width() * GROUND_SCALE
-		ground1.position = new_position
-	if (!visibilityNotifier2.is_on_screen()):
-		var new_position = ground1.position
-		new_position.x += ground1.texture.get_width() * GROUND_SCALE
-		ground2.position = new_position
-	if (!visibilityNotifier3.is_on_screen()):
-		var new_position = ground2.position
-		new_position.x += ground2.texture.get_width() * GROUND_SCALE
-		ground3.position = new_position
-
-func _ready():
-	visibilityNotifier1.connect("screen_exited", self, "_on_VisibilityNotifier2D_screen_exited")
-	visibilityNotifier2.connect("screen_exited", self, "_on_VisibilityNotifier2D_screen_exited")
-	visibilityNotifier3.connect("screen_exited", self, "_on_VisibilityNotifier2D_screen_exited")
+func moveGround(id: int):
+	match(id):
+		1:
+			var new_position = ground3.position
+			new_position.x += ground3_sprite.texture.get_width() * GROUND_SCALE
+			ground1.position = new_position
+		2: 
+			var new_position = ground1.position
+			new_position.x += ground1_sprite.texture.get_width() * GROUND_SCALE
+			ground2.position = new_position
+		3: 
+			var new_position = ground2.position
+			new_position.x += ground2_sprite.texture.get_width() * GROUND_SCALE
+			ground3.position = new_position
 
 func _process(delta):
 	translation.x += -SPEED * delta
 	position = translation
 
-func _on_VisibilityNotifier2D_screen_exited():
-	moveGround()
-	pass # Replace with function body.
+func _on_VisibilityNotifier2D_screen_exited(id: int):
+	moveGround(id)
+	pass
