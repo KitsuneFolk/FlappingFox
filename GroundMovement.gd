6c5e76faf8564a0d8b21b5e0a2cc452c41c590ba
MrRuslanYT <pandacorpglobal@gmail.com>
2023-11-28 11:12:15 +0300

    Added enemy spawn every 2-4 seconds

--- GroundMovement.gd
+++ GroundMovement.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const GROUND_SCALE = 15
 const SPEED = 300
+
+onready var player: KinematicBody2D = get_node("/root/StaticBody2D/PlayerKinematicBody2D")
 onready var distance_label: Label = get_node("/root/StaticBody2D/Background/Label")
 onready var ground1: StaticBody2D = get_node("/root/StaticBody2D/Node2D/Ground1")
 onready var ground2: StaticBody2D = get_node("/root/StaticBody2D/Node2D/Ground2")
@@ -28,11 +30,20 @@ func moveGround(id: int):
 			new_position.x += ground2_sprite.texture.get_width() * GROUND_SCALE
 			ground3.position = new_position
 
+var summoned_enemy_time = Time.get_unix_time_from_system()
 func _process(delta):
 	position.x -= SPEED * delta
-	distance_label.text = "DISTANCE: %s" % abs((position.x / 200) as int)
-	pass
+	distance_label.text = "DISTANCE: %s" % abs((position.x / 200) as int) 
+	if (Time.get_unix_time_from_system() - summoned_enemy_time > rand_range(2, 4)):
+		summoned_enemy_time = Time.get_unix_time_from_system()
+		summonEnemy(-(position.x + 100))
 
 func _on_VisibilityNotifier2D_screen_exited(id: int):
 	moveGround(id)
 	pass
+
+func summonEnemy(x: int):
+	var new_enemy = $Area2D.duplicate()
+	new_enemy.position.x = x
+	add_child(new_enemy)
+	print("Summoned, x = ", x, "current pos = ", position.x)
