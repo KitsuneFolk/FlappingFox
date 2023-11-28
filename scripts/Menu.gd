fd683724aa9ff96af842659b119d1b2346c28aaf
MrRuslanYT <pandacorpglobal@gmail.com>
2023-11-28 12:05:59 +0200

    Added Exit button to the menu

--- scripts/Menu.gd
+++ scripts/Menu.gd
@@ -2,6 +2,10 @@ extends Control
 
 func _ready():
 	$NewGameButton.connect("pressed", self, "_on_PlayButton_pressed")
+	$ExitButton.connect("pressed", self, "_on_ExitButton_pressed")
 
 func _on_PlayButton_pressed():
 	get_tree().change_scene("res://Game.tscn")
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
